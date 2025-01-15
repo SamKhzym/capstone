05b1ca20156558c5538bdba0b01355ba1777ca46
Sam Khzym <samuel.khzym@gmail.com>
2025-01-15 15:52:08 -0500

    ACC restructure + cleanup

--- src/controller/acc/include/ACC.h
+++ src/controller/acc/include/ACC.h
@@ -5,16 +5,22 @@
  * be used by ACC.c
  * */
 
+#include <stdint.h>
+#include <stdbool.h>
+
 //structs
-typedef struct pidParams {
+typedef struct PidParams {
     float P;
     float I;
     float D;
-} pidParams;
+    float errorSum;
+    float lastError;
+} PidParams;
 
-struct accParams {
-    pidParams speedPid;
-};
+struct AccParams {
+    float maxLeadDist;
+    PidParams speedPid;
+} AccParams;
 
 //enums
 enum Mode {
@@ -22,27 +28,13 @@ enum Mode {
     FOLLOW
 };
 
-enum Function {
-    CONSTANT,
-    LINEAR,
-    STEPWISE,
-    PARABOLIC,
-    SINUSOIDAL
-};
+const float dt = 0.01f;
 
-//Calibratables
-const float timeStep = 0.1; //Time step of system
-const float simTime = 20.0; //Simulation time in seconds
+// function headers
+bool leadVehicleExists(float leadDist, float leadSpeed, float setSpeed);
+void initPidParams(PidParams* params, float P, float I, float D);
+float pidStep(PidParams* params, float err);
+uint8_t saturateActReq(uint8_t actReq);
 
-//funtion headers
-void vehDynamics(float torque);
-void initACC(float* errorPrev, float* derivative);
-float accAlgo(float* hostVelInit, float leadVel, float setSpeed, float leadDist);
-void leadDynamics(float* dynamics, float timeStamp, float leadDistInit, float leadVelInit, enum Function mode);
-
-bool determineLeadVehicleExists(float leadDistance, float leadSpeed, float setSpeed);
-void initPidParams(struct PidParams* params, float P, float I, float D);
-float pidStep(struct PidParams* params, float err);
-void initACC();
-uint8_t accController(float leadSpeed, float leadDistance, float egoSpeed, float setSpeed);
-uint8_t saturateOutputCommand(uint8_t actReq, uint8_t actReqPrev);
+void initAcc();
+float stepAcc(float hostVel, float leadVel, float setSpeed, float leadDist);
--- src/controller/acc/src/ACC.c
+++ src/controller/acc/src/ACC.c
@@ -4,136 +4,82 @@
  * Modified By: Pesara A.
  * Description: Implements and ACC control algorithm
  * */
-#include<stdio.h>
 
 #include "ACC.h"
 
-//Globals
-float hostVel; // ---- acc params
-float hostVelPrev; //Host velocity in the previous time step ---- acc params
-float integral = 0; //Needs to be global as it is used in the calculation of next step ---- pid params
-float errorPrev = -999.0; //Previous value of PID error ---- pid params
-float leadDistPrev = 0;// ---- acc params
-float overshoot = 0;// ---- test/debug params 
-float leadTime = 0;// ---- test/debug params
+//Globals and parameters
+struct AccParams accParams;
 
-void vehDynamics(float torque){
-    //torque will be a value from -1 to 1, where negative values represent braking torque and positive values represent acceleration
-
-    //Calculate drag
-    //CdA = 0.27
-    float CdA = 0.55; //Default value for Drag Area
-    float rho = 1.293; //Default for air density
-    float mass = 1670.0; //Default of mass of vehicle
-
-    float drag = -0.5*rho*CdA*hostVel*hostVel/mass; //Deceleration due to drag
-
-    float hostAccel;
-    if (torque >= 0){ //Braking is more powerful than acceleration. Will this break the PID? Probably
-        hostAccel = drag + torque*4; //Assuming max acceleration is 4m/s; may want to make this a lookup table at some point
-    }
-    else if(torque < 0){
-        hostAccel = drag + torque*7; //Assuming max deceleration is 7m/s; may want to make this a lookup table at some point
-    }
-    
-    hostVel += hostAccel*timeStep; //Iterate the host velocity based on acceleration
-
-    //Prevent negative velocity
-    if (hostVel < 0){
-        hostVel = 0;
-    }
+bool leadVehicleExists(float leadDist, float leadSpeed, float setSpeed) {
+    return leadSpeed < setSpeed && leadDist < accParams.maxLeadDist;
 }
 
-void initACC(float* errorPrev, float* derivative){
-    if (*errorPrev == -999.0){ //AKA first time step
-        *derivative = 0;
-    }
+void initPidParams(PidParams* params, float P, float I, float D) {
+    params->P = P;
+    params->I = I;
+    params->D = D;
+    params->errorSum = 0.0f;
+    params->lastError = 0.0f;
 }
 
-float accAlgo(float hostVel, float leadVel, float setSpeed, float leadDist){
-    //Decide between cruise and follow modes
-    enum Mode mode; //0 = cruise, 1 = follow
-    if (leadVel > setSpeed || leadDist > 500){ //Future additions: safety critical mode, failure mode
-        mode = CRUISE;
-        //Do nothing, this algorithm defaults in cruise mode
+float pidStep(PidParams* params, float err) {
+
+    float pTerm = err * params->P;
+
+    params->errorSum += err;
+    float iTerm = params->errorSum * params->I;
+
+    float derivative = (err - params->lastError) / dt;
+    params->lastError = err;
+    float dTerm = derivative * params->D;
+
+    return pTerm + iTerm + dTerm;
+
+}
+
+void initAcc() {
+
+    float speedKp = 3.0f;
+    float speedKi = 0.05f;
+    float speedKd = 0.04f;
+
+    initPidParams(&accParams.speedPid, speedKp, speedKi, speedKd);
+    accParams.maxLeadDist = 5.0f;
+
+}
+
+uint8_t saturateActReq(uint8_t actReq) {
+
+    uint8_t saturated = actReq;
+
+    if (actReq < 50){
+        saturated = 0;
     }
-    else{
+    else if (actReq < 100){
+        saturated = 100;
+    }
+
+    return saturated;
+
+}
+
+float stepAcc(float hostVel, float leadVel, float setSpeed, float leadDist){
+
+    // Decide between cruise and follow modes
+    enum Mode mode = CRUISE; // defaults in cruise state
+
+    if (leadVehicleExists(leadDist, leadVel, setSpeed)) { //Future additions: safety critical mode, failure mode
         mode = FOLLOW;
         setSpeed = leadVel;
     }
     
     //Calculate PID terms
-    float error = setSpeed - hostVel; //P
-    integral += error*timeStep; //I
-    float derivative = (error - errorPrev)/timeStep; //D
+    float error = setSpeed - hostVel;
+    uint8_t actReq = (uint8_t)pidStep(&accParams.speedPid, error);
 
-    //Set PID parameters
-
-    pidParams pid;
-    pid.P = 3;
-    pid.I = 0.05;
-    pid.D = 0.04;
-
-    //Remove derivative from first time step
-    initACC(&errorPrev, &derivative);
-
-    //Set previous values for next time step
-    errorPrev = error;
-
-    //Implement PID controller
-    uint8_t actReq = pid.P * error + pid.I * integral + pid.D * derivative;
-
-    //Saturate actuation request value
-    
-    if (actReq < 50){
-        actReq = 0;
-    }
-    else if (actReq < 100){
-        actReq = 100;
-    }
-
-    // //Calculate overshoot, lead time, and steady state error
-    // if (((*hostVelInit < setSpeed) && (error < overshoot)) || ((*hostVelInit > setSpeed) && (error > overshoot))){
-    //     overshoot = error;
-    // }
-    // if (((*hostVelInit < setSpeed) && error > 0) || ((*hostVelInit > setSpeed) && error < 0)){
-    //     leadTime += timeStep;
-    // }
-
-
-    // vehDynamics(actReq); //Recalculate vehicle speed
-
-    //printf("%07.3f | %07.3f | %07.3f | %07.3f | %07.3f| %d\n", error, integral, derivative, torque, hostVel, mode);
-    // printf("%07.3f | %07.3f | %07.3f | %07.3f | %07.3f| %d\n", error, torque, hostVel*3.6, leadDist, leadVel*3.6, mode);
+    // pseudo-saturation
+    saturateActReq(actReq);
 
     return actReq;
-}
 
-
-
-void leadDynamics(float* dynamics, float timeStamp, float leadDistInit, float leadVelInit, enum Function mode){
-    //0 = Constant, 1 = Linear, 2 = Stepwise, 3 = Parabolic, 4 = Sinusoidal
-    
-    if (timeStamp == 0.0){
-        leadDistPrev = leadDistInit;
-    }
-
-    float leadVel;
-    float leadDist;
-
-    if (mode == CONSTANT){ //Constant
-        leadVel = leadVelInit;
-    }
-
-    if (mode == LINEAR){ //Linear
-        float slope = -1; //CAL
-        leadVel = leadVelInit + slope*timeStamp;
-    }
-
-    //Update lead distance based on relative velocity
-    leadDist = leadDistPrev + (leadVel - hostVel) * timeStamp;
-    leadDistPrev = leadDist;
-
-    dynamics[0] = leadVel;
-    dynamics[1] = leadDist;
 }
