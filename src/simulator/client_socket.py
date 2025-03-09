f47e3697128c3c894789dac1b6d74cbeba2a1088
Daniel Lupas <lupasd1@mcmaster.ca>
2025-03-08 23:27:59 -0500

    Added TCP connection code to hil

---
+++ src/simulator/client_socket.py
@@ -0,0 +1,58 @@
+import socket
+import struct
+import re
+
+class Socket():
+    
+    def __init__(self, HOST, PORT):
+        # Create a socket object for IPv6
+        self.client_socket = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+        
+        self.client_socket.connect((HOST, PORT, 0, 0))  # The last two arguments are for IPv6-specific address info
+        self.client_socket.settimeout(10.0)
+
+        print(f"Connected to {HOST} on port {PORT}")
+    
+    def close_connection(self):
+        self.client_socket.close()
+        print("Connection closed")
+
+    def send_environment_info(self, lead_speed, set_speed, lead_distance):
+        
+        payload = "<{:.3f}|{:.3f}|{:.3f}>".format(lead_speed, lead_distance, set_speed)
+        
+        self.client_socket.send(payload.encode('utf-8'))
+
+    def send_speed_request(self):
+        
+        payload = "230"
+        
+        self.client_socket.send(payload.encode('utf-8'))
+    
+    def receive_confirmation_message(self):
+        
+        #TODO Check recv payload size
+        payload = self.client_socket.recv(128)
+        
+        decoded_str = payload.decode('utf-8', errors='ignore')
+
+        print(decoded_str)
+        
+    def receive_vehicle_speed(self):
+        
+        #TODO Check recv payload size
+        payload = self.client_socket.recv(16)
+        
+        decoded_str = payload.decode('utf-8', errors='ignore')
+
+        # Extract the numeric part (assuming it's enclosed in '<' and '>')
+        match = re.search(r'<([\d\.]+)>', decoded_str)
+        if match:
+            vehicle_speed = float(match.group(1))
+        else:
+            print("No valid float found in payload.")
+        
+        # payload = payload.decode().strip('<>\x00\x08')
+        
+        # vehicle_speed = float(payload)
+        return vehicle_speed
--- src/simulator/hil.py
+++ src/simulator/hil.py
@@ -1,5 +1,6 @@
 from env_simulator import EnvironmentalSimulator, DT_SIM
 from real_time_viz import RealTimeViz, DT_VIZ
+from client_socket import Socket
 from pathlib import Path
 from time import time
 
@@ -12,6 +13,10 @@ DRIVE_CYCLE_NAME = 'hwfet.csv'
 SET_SPEED_MPS = 0.85
 INIT_LEAD_DIST_M = 1.0
 
+# Ethernet connection params
+HOST = "fd12:3456:789a::1"
+PORT = 4242
+
 def main():
 
     dt = DT_SIM
@@ -20,32 +25,54 @@ def main():
 
     drive_cycle_path = str(DRIVE_CYCLE_DIR / DRIVE_CYCLE_NAME)
     simulator = EnvironmentalSimulator(drive_cycle_path, SET_SPEED_MPS, INIT_LEAD_DIST_M)
-    viz = RealTimeViz()
+    
+    socket = Socket(HOST, PORT)
 
+    viz_enable = False
+    if viz_enable:
+        viz = RealTimeViz()
     doViz = True
-    while True:
+    
+    try:
+        while True:
 
-        # loop around until enough time has passed (sample time from last execution)
-        while (elapsed_time > 0 and (time() - (start_time + elapsed_time)) < dt): pass
-        elapsed_time = time() - start_time
+            # loop around until enough time has passed (sample time from last execution)
+            while (elapsed_time > 0 and (time() - (start_time + elapsed_time)) < dt): pass
+            elapsed_time = time() - start_time
 
-        # TODO: GET EGO SPEED FROM MICROCONTROLLER
-        ego_speed = 0.2
+            socket.send_speed_request()
+            # print("Waiting on vehicle speed...")
+            # ego_speed = socket.receive_vehicle_speed()
+            # print(f"Received vehicle speed: {ego_speed} m/s")
+            # TODO: GET EGO SPEED FROM MICROCONTROLLER
+            ego_speed = 0.2
+            
+            # update positions of vehicles in simulator
+            simulator.step_simulator(ego_speed)
+            lead_speed = simulator.get_current_lead_speed()
+            lead_distance = simulator.get_rel_lead_distance()
+            
+            if viz_enable:
+                # invoke visualizer every other timestep (dt_viz = 2*dt_sim)
+                doViz = not doViz
+                if doViz:
+                    viz.draw(
+                        simulator.ego_position, 
+                        ego_speed, 
+                        lead_speed,
+                        lead_distance
+                    )
 
-        # update positions of vehicles in simulator
-        simulator.step_simulator(ego_speed)
-
-        # invoke visualizer every other timestep (dt_viz = 2*dt_sim)
-        doViz = not doViz
-        if doViz:
-            viz.draw(
-                simulator.ego_position, 
-                ego_speed, 
-                simulator.get_current_lead_speed(),
-                simulator.get_rel_lead_distance()
-            )
-
-        # TODO: transmit stuff to microcontroller
+            # TODO: transmit stuff to microcontroller
+            socket.receive_confirmation_message()
+            # print("Sending environmental info to board...")
+            # socket.send_environment_info(lead_speed, SET_SPEED_MPS, lead_distance)
+            
+    # except Exception as e:
+    #     print(e)
+    finally:
+        socket.close_connection()
+        
 
 if __name__ == '__main__':
     main()
