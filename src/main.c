b74a14cbb1b7dc99146394a1a78c4b5973b162fb
Pesara Amarasekera <amarasep@mcmaster.ca>
2025-03-10 14:28:46 -0400

    Made the tests the all work! woo hoo

--- src/main.c
+++ src/main.c
@@ -6,9 +6,24 @@
 
 //test body
 int main(){
-    uint8_t message[] = {0x32,0x12};
-    uint8_t crc = crc8(message, sizeof(message));  // Poly: 0x07 (CRC-8 standard)
-    
-    printf("CRC-8: 0x%02X\n", crc);  // Print 8-bit CRC
+    uint8_t message1[] = {0x32,0x12};
+    uint8_t crc1 = crc8(message1, sizeof(message1));  // Poly: 0x07 (CRC-8 standard)
+    printf("CRC-8: 0x%02X\n", crc1);  // Print 8-bit CRC
+
+    uint8_t message2[] = {0x00,0x00,0x00};
+    uint8_t crc2 = crc8(message2, sizeof(message2));  // Poly: 0x07 (CRC-8 standard)
+    printf("CRC-8: 0x%02X\n", crc2);
+
+    uint8_t message3[] = {0x12,0x34,0x56,0x78};
+    uint8_t crc3 = crc8(message3, sizeof(message3));  // Poly: 0x07 (CRC-8 standard)
+    printf("CRC-8: 0x%02X\n", crc3);
+
+    uint8_t message4[] = {0x11,0x11,0x11,0x11};
+    uint8_t crc4 = crc8(message4, sizeof(message4));  // Poly: 0x07 (CRC-8 standard)
+    printf("CRC-8: 0x%02X\n", crc4);
+
+    uint8_t message5[] = {0x31};
+    uint8_t crc5 = crc8(message5, sizeof(message5));  // Poly: 0x07 (CRC-8 standard)
+    printf("CRC-8: 0x%02X\n", crc5);
     return 0;
 }
--- test/crc-test.c
+++ test/crc-test.c
@@ -4,6 +4,5 @@
 
 TEST(CRC32Test, BasicCases){
     uint8_t data1[] = {0x00, 0x00, 0x00, 0x00};
-    
 }
 
