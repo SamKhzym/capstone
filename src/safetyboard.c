0e3ef29f07ff06fbd6d12cccf9fa1a45ad518218
Pesara Amarasekera <amarasep@mcmaster.ca>
2025-03-15 18:08:37 -0400

    Added WIP padded CRC for better security

--- src/safetyboard.c
+++ src/safetyboard.c
@@ -13,11 +13,45 @@
 #include "safetyboard.h"
 //x^7 + x^5 + x^3 + 1 <=> 1010101
 
+
+//private for padding messages before doing crc
+uint8_t* padMessage(uint8_t* message, size_t len){
+    uint8_t* paddedMsg = malloc((len+DEGPOLY)*sizeof(uint8_t));
+    for (size_t i = 0; i < len; i++){
+        paddedMsg[i] = message[i];
+        #if DEBUG
+        printf("0x%02X 0x%02X\n",paddedMsg[i], message[i]);
+        #endif
+    }
+    for (uint8_t i = len; i <len+DEGPOLY;  i++){
+        paddedMsg[i] = 0; 
+    }
+    return paddedMsg;
+}
+
 /** CRC check based on the following
  * https://barrgroup.com/blog/crc-series-part-3-crc-implementation-code-cc
  *
  *
  */
+
+uint8_t  paddedCRC(uint8_t* message, size_t len){
+    message = padMessage(message,len);
+    len = len+DEGPOLY;
+    uint8_t crc = 0x00;
+    for (int byte = 0; byte < len; byte++){
+        crc ^= message[byte];
+        for (uint8_t bit = 8; bit > 0; bit--){
+            if (crc & TOPBIT){
+                crc = (crc << 1) ^ POLYNOMIAL;
+            }else{
+                crc = (crc << 1);
+            }
+        }
+    }
+    return crc; 
+}
+
 uint8_t crc8(uint8_t* message, size_t len){
     uint8_t crc = 0x00;
     for (int byte = 0; byte < len; byte++){
